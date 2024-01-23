7363dd4e9cfe831bd99681b2ad14fd0f68654322
Giovandro Santos <vsm.giovandro@gmail.com>
2024-01-23 08:05:57 -0300

    first commit

---
+++ src/main/kotlin/dev/camila/request/credit/system/RequestCreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package dev.camila.request.credit.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestCreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestCreditSystemApplication>(*args)
+}
